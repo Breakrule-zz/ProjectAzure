1d197933900994825cde6fc3ac25efb95410a280
Rezky Maulana <30544168+Breakrule@users.noreply.github.com>
2020-01-14 04:31:52 +0700

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://salesforecasting.azurewebsites.net'
+url = 'https://salesforecasting.azurewebsites.net:5000'
 r = requests.post(url,json={'rate':5, 'sales_in_first_month':200, 'sales_in_second_month':400})
 
 print(r.json())
